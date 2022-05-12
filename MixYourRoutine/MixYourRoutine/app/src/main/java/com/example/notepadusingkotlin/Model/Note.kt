c7cc5f45f71f061d8e7b30a412955f1fcd2c2ed4
Sultan <sultanahmullaev@mail.ru>
2022-05-12 12:41:49 +0600

    'Changes'

--- MixYourRoutine/MixYourRoutine/app/src/main/java/com/example/notepadusingkotlin/Model/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.example.notepadusingkotlin.Model
-
-class Note(noteID: Int, noteName: String, noteDes: String)
-{
-    var noteID : Int? = noteID
-    var noteName : String? = noteName
-    var noteDes : String? = noteDes
-}
